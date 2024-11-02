81d80a03cc0fa6e14207fe9386b7551a6785c584
Ekta_Gupta <108565384+01Ekta@users.noreply.github.com>
2024-11-03 00:55:02 +0530

    Time: 2 ms (51.29%) | Memory: 22 MB (9.05%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,8 +3,11 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
+
+        # we use two-pointer 
         i, j = 0, len(s) - 1
-        while i < j:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
+        while i < j:   # here if length of string is even, then character swapped easily
+        # but string length is odd, then middle element will stay at position in string as well reversed string and other element would be swapped
+            s[i], s[j] = s[j], s[i]     # swap pointer character
+            i += 1                      # increment 1st pointer by 1
+            j -= 1                      # decrement 1st pointer by 1 
